84f5493374fec643cb84d01a56bc4784ce690c6f
mruilopbc <murilopoltronieri3@gmail.com>
2024-03-15 10:01:05 -0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -19,99 +19,60 @@ public class Main {
                 if (opt == 1) {
                     while (true) {
                         try {
-                            System.out.println("Digite a posição inicial do trem A: ");
+                            System.out.println("\nDigite a posição inicial do trem A: ");
                             posicaoTremA = sc.nextDouble();
-
-                            if (posicaoTremA < posicaoInicial || posicaoTremA >= posicaoFinal) {
-                                System.out.println("Posição inválida do Trem A!");
-                                continue;
-                            }
-                            break;
-
-                        } catch (InputMismatchException e) {
-                            System.out.println("Valor Inválido!");
-                            sc.next();
-                        }
-                    }
-
-                    while (true) {
-                        try {
-                            System.out.println("Digite a velocidade do trem A em Km/h: ");
-                            velocidadeTremA = sc.nextDouble();
-
-                            if (velocidadeTremA > 300 || velocidadeTremA < 0) {
-                                System.out.println("Digite um valor entre 0 a 300");
-                                continue;
-                            }
-                            posicaoTremA += velocidadeTremA;
-                            System.out.println("KM atual do trem A: " + posicaoTremA);
-                            break;
-
-                        } catch (InputMismatchException e) {
-                            System.out.println("Você digitou um caractere inválido!");
-                            sc.next();
-                        }
-                    }
-
-                    while (true) {
-                        try {
                             System.out.println("\nDigite a posição inicial do Trem B: ");
                             posicaoTremB = sc.nextDouble();
 
-                            if (posicaoTremB <= posicaoInicial || posicaoTremB > posicaoFinal || posicaoTremB <= posicaoTremA) {
-                                System.out.println("Posição inválida do Trem B");
+                            if (posicaoTremA < posicaoInicial || posicaoTremB < posicaoInicial || posicaoTremA > posicaoFinal || posicaoTremB > posicaoFinal) {
+                                System.out.println("\nDigite uma posição entre 0 e 10000!");
                                 continue;
                             }
-                            break;
+                            if (posicaoTremB <= posicaoTremA) {
+                                System.out.println("\nOs trens não colidiram!");
+                                continue;
+                            }
+                            while (true) {
+                                try {
+                                    System.out.println("\nDigite a velocidade do trem A em Km/h: ");
+                                    velocidadeTremA = sc.nextDouble();
+                                    System.out.println("\nDigite a velocidade do trem B em Km/h: ");
+                                    velocidadeTremB = sc.nextDouble();
 
-                        } catch (InputMismatchException e) {
+                                    if (velocidadeTremA > 300 || velocidadeTremA < 0 || velocidadeTremB < -300 || velocidadeTremB > 0) {
+                                        System.out.println("\nDigite um valor entre 0 a 300, sendo que a velocidade do trem B deve ser negativo!");
+                                    }
+                                    if (velocidadeTremA == 0 && velocidadeTremB == 0 ){
+                                        System.out.println("\nOs trens não colidiram!");
+                                    }
+
+                                    // Lógica colisão
+
+                                } catch (InputMismatchException e) {
+                                    System.out.println("\nVocê digitou um caractere inválido!");
+                                    sc.next();
+
+                                }
+                            }
+                        } catch (NumberFormatException e) {
                             System.out.println("Valor Inválido!");
                             sc.next();
                         }
                     }
 
-                    while (true) {
-                        try {
-                            System.out.println("Digite a velocidade do trem B em Km/h: ");
-                            velocidadeTremB = sc.nextDouble();
-
-                            if (velocidadeTremB < -300 || velocidadeTremB > 0) {
-                                System.out.println("Digite um valor entre 0 e -300!");
-                                continue;
-                            }
-                            posicaoTremB += velocidadeTremB;
-                            System.out.println("KM atual do trem B " + posicaoTremB);
-                            sc.next();
-                            break;
-
-                        } catch (InputMismatchException e) {
-                            System.out.println("Você digitou um caractere inválido!");
-                            sc.next();
-                        }
-                    }
-
-                    if (posicaoTremB <= posicaoTremA ){
-                        System.out.printf("A colisão de trens acontecerá no KM %f e ocorrerá", posicaoTremB);
-                    }
-                    if (velocidadeTremA == 0 && velocidadeTremB == 0 ){
-                        System.out.println("Os trens não colidiram!");
-                        break;
-                    }
-
                 }
-
                 if (opt == 2) {
                     System.out.println("FIM DO PROGRAMA!");
                     break;
 
                 } else {
                     System.out.println("Digite 1 ou 2!\n");
-
                 }
             } catch (NumberFormatException e) {
-                System.out.println("\nValor Inválido!\n");
+                System.out.println("\nValor Inválido\n");
 
             }
+
         }
     }
 }
